fac64e8caf7bb55390953f569dc0a01b2d88bf03
Ekaterina Tarasova <tarasova@tlu.ee>
2023-12-30 16:28:37 +0700

    Начальная логика на JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  cardsContainer.append(cardElement);
+}
+
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
+}
+
+initialCards.forEach(function (card) {
+  createCard(card, deleteCard);
+})
