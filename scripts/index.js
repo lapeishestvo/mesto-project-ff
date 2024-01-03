bf0a258f3d35529789791668d05ea8ba9e9420b4
Ekaterina Tarasova <tarasova@tlu.ee>
2024-01-03 17:35:56 +0700

    Правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -12,11 +12,12 @@
 const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard(card, deleteCard) {
+function createCard(card) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = card.name;
   cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = `Фотография с видами региона ${card.name}`;
   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
 
   cardsContainer.append(cardElement);
